525dc43c2d7893be75d90e012568d7ca2a85cf5e
bob <657793418@qq.com>
2020-12-04 20:13:17 +0800

    第一个实验的初始helloworld 代码 明天搞第一个实验

---
+++ code/experiment1/app/src/main/java/haonan/tech/experiment1/MainActivity.kt
@@ -0,0 +1,11 @@
+package haonan.tech.experiment1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
